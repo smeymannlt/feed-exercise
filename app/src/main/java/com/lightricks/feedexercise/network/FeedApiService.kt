57c380b1273f241229857c9f0e92176806b06a5f
smeymannlt <smeymann@lightricks.com>
2020-08-25 16:03:13 +0300

    First working version of online gallery

--- app/src/main/java/com/lightricks/feedexercise/network/FeedApiService.kt
+++ app/src/main/java/com/lightricks/feedexercise/network/FeedApiService.kt
@@ -1,5 +1,41 @@
 package com.lightricks.feedexercise.network
 
-/**
- * todo: add the FeedApiService interface and the Retrofit and Moshi code here
- */
+import android.net.Uri
+import androidx.annotation.WorkerThread
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+internal interface FeedFetcher {
+    @GET("Android/demo/feed.json")
+    fun fetch(): Single<FeedData>
+}
+
+class FeedApiService {
+    @WorkerThread
+    fun fetchStream(): Single<FeedData> {
+        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+        val retrofit = Retrofit.Builder().baseUrl("https://assets.swishvideoapp.com/")
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+            .addConverterFactory(MoshiConverterFactory.create(moshi)).build()
+
+        return retrofit.create(FeedFetcher::class.java).fetch()
+    }
+
+    companion object {
+        fun uriForItem(item: FeedData.Metadata) = if (item.templateThumbnailUrl.isNotBlank()) {
+            Uri.parse("https://assets.swishvideoapp.com/Android/demo/catalog/thumbnails/")
+                .buildUpon()
+                .appendPath(item.templateThumbnailUrl).build()
+        } else {
+            Uri.EMPTY
+        }
+
+        private const val LOG_TAG = "FeedApiService"
+    }
+}
